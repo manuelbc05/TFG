efd7639b895cbf355ee38991e4c57dafc69377f3
manuelbc_05 <manuelbravocandilejo@gmail.com>
2026-03-09 18:07:17 +0100

    Funciona la API perfectamente ya, por lo que puedo hacer limpieza de código basura y dejar todo más limpio

--- app/src/main/java/com/example/tfg/HomeFragment.kt
+++ app/src/main/java/com/example/tfg/HomeFragment.kt
@@ -1,11 +1,9 @@
 package com.example.tfg
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.tfg.databinding.FragmentHomeBinding
@@ -13,41 +11,19 @@ import com.example.tfg.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Infla el layout
+    ): View {
+
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        // Configura el WebView
         val webView = binding.webview
-        webView.settings.javaScriptEnabled = true // Habilitar JavaScript
-        webView.settings.domStorageEnabled = true // Habilitar almacenamiento local
-        webView.settings.setSupportZoom(true) // Habilitar zoom
-        webView.settings.useWideViewPort = true // Usar el viewport adecuado
-        webView.settings.loadWithOverviewMode = true // Ajustar el contenido al viewport
-        WebView.setWebContentsDebuggingEnabled(true)
-        // Log para verificar si el WebView está siendo configurado correctamente
-        Log.d("WebView", "Configuración de WebView: JavaScript habilitado y zoom habilitado")
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
 
-        // Configura WebViewClient para capturar errores de carga
-        webView.webViewClient = object : WebViewClient() {
-            override fun onReceivedError(view: WebView?, errorCode: Int, description: String?, failingUrl: String?) {
-                super.onReceivedError(view, errorCode, description, failingUrl)
-                // Log para errores de carga
-                Log.e("WebViewError", "Error al cargar el mapa: $description")
-            }
-        }
-
-
-        // Verifica la URL que se está cargando
-        Log.d("WebView", "Cargando URL: file:///android_asset/map.html")
-
-        // Cargar el archivo HTML desde assets
+        webView.webViewClient = WebViewClient()
         webView.loadUrl("file:///android_asset/map.html")
-
-        // Log de verificación de carga
-        Log.d("WebView", "Archivo map.html cargado en WebView")
 
         return binding.root
     }
